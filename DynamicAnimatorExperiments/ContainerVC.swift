3e6b4c4289bb5d62e711584c73673ce984223721
Andrii Kravchenko <kievkao@gmail.com>
2015-10-26 13:04:25 +0200

    Prepare for side dynamic menu implementation

---
+++ DynamicAnimatorExperiments/ContainerVC.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerVC.swift
+//  DynamicAnimatorExperiments
+//
+//  Created by Andrii Kravchenko on 10/26/15.
+//  Copyright © 2015 Andrii Kravchenko. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ DynamicAnimatorExperiments/TopVC.swift
@@ -0,0 +1,17 @@
+//
+//  TopVC.swift
+//  DynamicAnimatorExperiments
+//
+//  Created by Andrii Kravchenko on 10/26/15.
+//  Copyright © 2015 Andrii Kravchenko. All rights reserved.
+//
+
+import UIKit
+
+class TopVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
